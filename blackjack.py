8bc16945cdc60b638da69ec83785fb2a2ac09ddd
restodruid1 <106450663+restodruid1@users.noreply.github.com>
2023-01-19 11:48:34 -0800

    Re saving hit or stand

--- blackjack.py
+++ blackjack.py
@@ -94,4 +94,19 @@ def hit_or_stand(deck,hand):
             print("Sorry, please try again.")
             continue
         break
+
+def player_busts(player,dealer,chips):
+    print("Player busts")
+    chips.lose_bet()
+def player_wins(player,dealer,chips):
+    print("Player wins")
+    chips.win_bet()
+def dealer_busts(player,dealer,chips):
+    print("Dealer busts")
+    chips.win_bet()
+def dealer_wins(player,dealer,chips):
+    print("Dealer wins")
+    chips.lose_bet()
+def push(player,dealer):
+    print("Dealer and Player tie. It's a push")
             
