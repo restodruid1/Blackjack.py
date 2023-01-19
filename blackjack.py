4ba5b8cabe13140daddffe35ced598c88026e086
restodruid1 <106450663+restodruid1@users.noreply.github.com>
2023-01-19 11:03:54 -0800

    First real code of this project

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,22 @@
-def say_hello(word):
-    print(word)
+#A game of Blackjack
+import random
+suits = ("Hearts", "Clubs", "Diamonds", "Spades")
+ranks = ("Two", "Three", "Four", "five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace")
+values = {"Two":2, "Three":3, "Four":4, "five":5, "Six":6, "Seven":7, "Eight":8, "Nine":9, "Ten":10, "Jack":10, "Queen":10, "King":10, "Ace":11}
+
+playing = True
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+class Deck:
+    def __init__(self):
+        self.deck = []
 
 
-say_hello("hello")
-say_hello("Yo")
-say_hello("hi")
+print(values)
